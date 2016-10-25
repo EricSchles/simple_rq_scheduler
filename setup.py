50d5fb7a8c2368a1fada05e66d45227ba7217963
Eric Schles <ericschles@gmail.com>
2016-10-25 13:14:26 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="simple_scheduler",
+      version="0.0",
+      description="a simple scheduler for rq",
+      author="Eric Schles",
+      packages=["simple_scheduler"]
+)
